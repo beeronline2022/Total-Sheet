9b0017703c6bca50b77dd5e5be79acd366ff8301
beeronline2022 <beer.online2022@gmail.com>
2026-09-09 09:02:52 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // วาง Web app URL ที่ deploy จาก Apps Script ของไฟล์ CS : TOSM 2026 ไว้ตรงนี้
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = 'https://script.google.com/macros/s/AKfycbxePy-cUUFNfPBbF9alIlDhTsg-wlObjJt1iZ1J-3DImnWDn_9NX2ADWnbi2_CyZmTf/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwOL5gtn7OZbw0cDF4iq4Xd2dd4Radrxbwa-gIT06uIc_6m5NNPvrE_IHa-rTQkzy75/exec';
  
 // รหัสเข้าถึง ต้องตรงกับค่าที่ตั้งไว้ใน Script Properties ชื่อ DASHBOARD_ACCESS_KEY
 const ACCESS_KEY = 'a07pxG9js452JLCQAc3F';
