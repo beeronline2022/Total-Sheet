e769edde6de3acd7f2085010b4554b4bb29b624d
CS-EXE-commits <beer.online2022@gmail.com>
2026-09-11 15:19:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // วาง Web app URL ที่ deploy จาก Apps Script ของไฟล์ CS : TOSM 2026 ไว้ตรงนี้
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = 'https://script.google.com/macros/s/AKfycby2OoB5VhYTPOXspAl176V_huIGpLB6palfQWnpn22JzWtsfqVyN9JHzYR7o-t8_An2/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwJwIBv9d-X0n5ua8Gkzz8S7AmxYlu2Y1lwFEF1Fb7gAZoNQ0_cK6TJtLYHowY_IwnX';
 
 // รหัสเข้าถึง ต้องตรงกับค่าที่ตั้งไว้ใน Script Properties ชื่อ DASHBOARD_ACCESS_KEY
 const ACCESS_KEY = 'a07pxG9js452JLCQAc3F';
